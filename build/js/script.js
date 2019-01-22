7ad6ed19b81116134d1970e26b97d45b94958931
Lyubov Mukhacheva <citymnk@gmail.com>
2019-01-22 23:23:17 +0700

    finished all sections and adptive version
    addede IDs in links to scroll through the page, tested adaptive and changed some properties for different resolutions.
    ToDo: load on gh-pages, tests on the real devices

--- build/js/script.js
+++ build/js/script.js
@@ -679,13 +679,19 @@ $(document).ready(function() {
 });
 
 
-/* Плавный переход к якорю */
 $(document).ready(function() {
+
+  /* Плавный переход к якорю */
   $(function(){
-    $("a[href^='#']").click(function(){
-      var _href = $(this).attr("href");
-      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    $('a[href^="#"]').click(function(){
+      var _href = $(this).attr('href');
+      $('html, body').animate({scrollTop: $(_href).offset().top+'px'});
       return false;
     });
+  });
+
+  /* Закрытие navbar по клику на пункт меню */
+  $('.nav-link').click(function(){
+    $('.navbar-collapse').removeClass('show');
   });
 });
--- js/script.js
+++ js/script.js
@@ -4,13 +4,19 @@
 //= preloader.js
 
 
-/* Плавный переход к якорю */
 $(document).ready(function() {
+
+  /* Плавный переход к якорю */
   $(function(){
-    $("a[href^='#']").click(function(){
-      var _href = $(this).attr("href");
-      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    $('a[href^="#"]').click(function(){
+      var _href = $(this).attr('href');
+      $('html, body').animate({scrollTop: $(_href).offset().top+'px'});
       return false;
     });
+  });
+
+  /* Закрытие navbar по клику на пункт меню */
+  $('.nav-link').click(function(){
+    $('.navbar-collapse').removeClass('show');
   });
 });
